35c13a65cdd31bb91e9ba111014fe31c7c2837aa
Alexis Josephides <ajosephides@gmail.com>
2019-09-04 17:00:18 +0100

    template: template read

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const fs = require('fs')
 const app = express();
 const port = process.env.PORT || "8888";
 
-const folderPath = __dirname + '/content'
-const endpoints = () => {return fs.readdirSync(folderPath).map(fileName => {return `/${fileName}`})}
+
+const endpoints = () => {
+  const folderPath = __dirname + '/content'
+  const folders = fs.readdirSync(folderPath);
+  return folders.map(fileName => {return `/${fileName}`});
+}
+
+let getTemplate = () => { return fs.readFileSync(__dirname + '/template.html', 'utf-8', function(err, data) {
+  if (err) {
+    console.log('Template not found ' + err);
+  } else{
+      return data;
+    }
+  });
+}
+
 
 app.get(endpoints(), (req, res) => {
   res.send('Hello world')
@@ -15,4 +29,4 @@ app.listen(port, () => {
   console.log(`Listening to requests on http://localhost:${port}`);
 });
 
-module.exports = {endpoints: endpoints}
+module.exports = {endpoints: endpoints, getTemplate: getTemplate}
--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,8 +1,24 @@
 const App = require('../../app')
+const fs = require('fs')
+
+jest.mock('fs', () => ({
+  readdirSync: jest.fn(() => {return ['about-page', 'jobs', 'valves']}),
+  readFileSync: jest.fn(() => {return '<html><head>Mock template<head><html>'})
+}));
 
 
 describe('App', () => {
-  it('has valid url routes that match expected content', () => {
-    expect(App.endpoints()).toEqual(['/about-page', '/jobs', '/valves'])
+
+  describe('#enpoint', () => {
+    it('has valid url routes that match expected content', () => {
+      expect(App.endpoints()).toEqual(['/about-page', '/jobs', '/valves'])
+    })
   })
+
+  describe('#getTemplate', () => {
+    it('finds template content', () => {
+      expect(App.getTemplate()).toEqual('<html><head>Mock template<head><html>')
+    })
+  })
+
 })
